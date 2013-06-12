aa377c9c0e536c08d638a75477b59297c2db87f1
Javier Aguirre <contacto@javaguirre.net>
2013-06-12 14:16:59 +0200

    Ordering timelog by day, week or month

--- timelog/utils.py
+++ timelog/utils.py
@@ -53,6 +53,7 @@ def calculate_report(filepath, start_date, end_date, order_by, client=None):
 
     with codecs.open(os.path.expanduser(filepath), encoding='utf-8') as filed:
         for line in filed:
+            # TODO Tasks
             try:
                 time, entry = line.split(': ', 1)
                 entry = entry.replace('\n', '')
@@ -69,49 +70,56 @@ def calculate_report(filepath, start_date, end_date, order_by, client=None):
     return entries
 
 
-def report_group_by(data, order_by):
-    if not data:
+def report_group_by(entries, order_by):
+    new_entries = {}
+    last_value = None
+
+    if not entries:
         return False
 
-    if order_by is 'month':
-        start = data[0][0].month
-    elif order_by is 'week':
-        start = data[0][0].isocalendar()[1]
+    if order_by in ('week', 'month', 'day'):
+        for date_work, client, duration in entries:
+            if order_by == 'week':
+                date_value = date_work.isocalendar()[1]
+            else:
+                date_value = getattr(date_work, order_by)
+
+            if last_value == date_value:
+                if client in new_entries[date_value]:
+                    new_entries[date_value][client][2] += duration
+                else:
+                    new_entries[date_value][client] = [date_work, client, duration]
+            else:
+                new_entries[date_value] = {client: [date_work, client, duration]}
+
+            last_value = date_value
     else:
-        start = data[0][0]
+        # FIXME This is not good because in this case new_entries is a list
+        new_entries = entries
+
+    return new_entries
 
 
-
-def get_price(entry, duration, clients_file, with_price=False):
-    if with_price:
-        with codecs.open(clients_file, encoding='utf-8') as f:
-            for line in f:
-                if line.startswith(entry.lower()):
-                    client_info = line.split(':')
-                    if client_info[0] == entry.lower():
-                        if client_info[1] == 'hour':
-                            hours = '%.2f' % round((duration.total_seconds()*1.0)/3600, 2)
-                            return '%s Euros' % str(int(client_info[2])*float(hours))
-                        elif client_info[1] == 'day':
-                            days = '%.2f' % round(((duration.total_seconds()*1.0)/3600)/8, 2)
-                            return '%s Euros' % str(int(client_info[2])*float(days))
-    return ''
-
-
-def print_result(data_report, filepath=None):
+def print_result(data_report, order_by=None, filepath=None):
     # TODO If filepath
-    for item in data_report:
-        print('%s %s %s' % (item[0], item[1], format_duration(item[2])))
+    if not order_by:
+        for item in data_report:
+            print('%s %s %s' % (item[0], item[1], format_duration(item[2])))
+    else:
+        for order_value, data in data_report.items():
+            print order_value
+            for client, client_info in data.items():
+                print(client, format_duration(client_info[2]))
 
 
 def run_timelog(args):
     filepath = args.timelog_src
     filepath_dst = None
 
-    data_report = calculate_report(filepath,
-                                   args.start_date, args.end_date,
-                                   args.order_by, args.client)
+    entries = calculate_report(filepath,
+                               args.start_date, args.end_date,
+                               args.order_by, args.client)
     if args.order_by and args.order_by in ('day', 'week', 'month'):
-        report_group_by(data_report, args.order_by)
+        entries = report_group_by(entries, args.order_by)
 
-    print_result(data_report, filepath_dst)
+    print_result(entries, args.order_by, filepath_dst)
